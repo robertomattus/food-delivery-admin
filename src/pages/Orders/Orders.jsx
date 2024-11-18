7bba5d8c32b2da4ddd6438c2df5d60361ee13ce0
robert <robertmatt113@gmail.com>
2024-11-18 14:41:52 -0600

    feat: update order status

--- src/pages/Orders/Orders.jsx
+++ src/pages/Orders/Orders.jsx
@@ -13,9 +13,20 @@ const Orders = ({ url }) => {
     const response = await axios.get(url + "/api/order/list");
     if (response.data.success) {
       setOrders(response.data.data);
-      console.log(response.data.data);
     } else {
       toast.error("Error");
+    }
+  };
+
+  const statusHandler = async (event, orderId) => {
+    const response = await axios.post(url + "/api/order/status", {
+      orderId,
+      status: event.target.value,
+    });
+    try {
+      await fetchAllOrders();
+    } catch (error) {
+      console.log(error);
     }
   };
 
@@ -60,7 +71,10 @@ const Orders = ({ url }) => {
               </div>
               <p>Items: {order.items.length}</p>
               <p>${order.amount}</p>
-              <select>
+              <select
+                onChange={(event) => statusHandler(event, order._id)}
+                value={order.status}
+              >
                 <option value="Food Processing">Food Processing</option>
                 <option value="Out for delivery">Out for delivery</option>
                 <option value="Delivered">Delivered</option>
